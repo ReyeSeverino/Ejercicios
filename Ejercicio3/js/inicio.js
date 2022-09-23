258ad466c4fa8a9494cf9894d38c115e02c07050
CarlosG <carlosguillermosrs@gmail.com>
2022-09-23 17:50:52 -0400

    codigo mejorado Por Raulin

--- Ejercicio3/js/inicio.js
+++ Ejercicio3/js/inicio.js
@@ -1,50 +1,40 @@
 function validarFormulario(inputId) {
-    var inputValue = document.getElementById(inputId).value;
+    var inputValue = $(inputId).value;
 
     var error = "";
     if (!inputValue) error = "El compo de " + inputId + " no puede estar vacio";
 
     switch (inputId) {
         case 'usuario':
-            if (inputValue.length >= 1 && inputValue.length < 5)
-            {
+            if (inputValue.length >= 1 && inputValue.length < 5) {
                 error = "el nombre de usuario debe tener de 5 o mas caracteres";
             }
             break;
-    
+
         case 'clave':
-            if (inputValue.length >= 1 && inputValue.length < 4)
-            {
+            if (inputValue.length >= 1 && inputValue.length < 4) {
                 error = "la contraseña es menor de 4 caracteres";
             }
-            else if(inputValue.length > 16)
-            {
+            else if (inputValue.length > 16) {
                 error = "la contraseña es mayor de 16 caracteres";
             };
             break;
-        
+
         default:
             console.log('validarFormulario error: id no valido.')
             break;
     }
 
-    document.getElementById(inputId + 'Span').innerText = error;
+    $(inputId + 'Span').innerText = error;
 };
 
-let inicioSecion = () => {
-    var usuario = document.getElementById('usuario').value;
-    var clave = document.getElementById('clave').value;
+function inicioSecion() {
+    var usuario = $('usuario').value;
+    var clave = $('clave').value;
 
     console.log(usuario, clave);
 }
 
-let muestra_mensaje = (mensaje) => { 
-    // alert(mensaje);
-    return mensaje;
-};
-
-window.onload = () => {
-    muestra_mensaje("Hola desde inicio.js")
-};
-muestra_mensaje("yo pase por aqui")
-
+function $(selector) {
+    return document.getElementById(selector);
+}
